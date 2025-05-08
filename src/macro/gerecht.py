92e487aacbd46b7832dfdde062e3f704bb4210ce
TomScheeper <tomscheeper@gmail.com>
2025-05-08 23:45:52 +0200

    gerecht

---
+++ src/macro/gerecht.py
@@ -0,0 +1,32 @@
+from typing import Any, Dict, List
+
+from .macrotype import Voedingswaarde
+
+
+class Gerecht:
+    
+    def __init__(
+        self,
+        gerecht_naam: str,
+        categorie_uuid: str, # dezelfde class kan gebruikt worden als voor product
+        producten: Dict[str, float], #  uuid: aantal 100 g/ml
+        porties: int,
+        recept: str,
+        versies: List[Dict[str, Any]], # uitvoeringen van het recept met een dictionary met een naam, [{"versie_naam": "standaard", "toegevoegd": {}, "verwijderd": [], "vervangen": {}, "hoeveelheid": {}}]
+        ) -> "Gerecht":
+        
+        self.gerecht_naam = gerecht_naam
+        self.categorie_uuid = categorie_uuid
+        self.producten = producten
+        self.porties = porties
+        self.recept = recept
+        self.versies = versies
+        
+    @property
+    def voedingswaarde(self) -> Voedingswaarde:
+        ...
+        
+    @property
+    def ingrediënten(self) -> Dict[str, str]: # mapping van product_uuid: ingredient_uuid
+        
+        ...
