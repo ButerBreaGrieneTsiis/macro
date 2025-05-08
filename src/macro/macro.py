739c3e4dc11447a7f6f010ac1ce16ba789448401
TomScheeper <tomscheeper@gmail.com>
2025-05-08 23:45:40 +0200

    core

---
+++ src/macro/macro.py
@@ -0,0 +1,31 @@
+from grienetsiis import open_json, opslaan_json
+# from .types import Voedingswaarde, Ingredient, Product
+
+def openen():
+    
+    ...
+    
+def opslaan():
+    
+    ...
+
+def initialiseren() -> None:
+    
+    # controleren of /gegevens/ bestaat
+    # controleren of alle .json bestaat
+    # zo nee:
+    # uitvoeren functies voor invullen .json (deze functies kunnen later ook gebruikt worden om te bewerken)
+    
+    ...
+    
+    opslaan()
+
+def uitvoeren():
+    
+    initialiseren()
+    openen()
+    
+    # dingen doen
+    
+    opslaan()
+    
