7a5c473fbbb2dfe1b7c39e2355a3452ae9f9522d
TomScheeper <tomscheeper@gmail.com>
2025-05-26 22:34:01 +0200

    dag export

--- src/macro/__init__.py
+++ src/macro/__init__.py
@@ -1,4 +1,5 @@
 from .categorie import Hoofdcategorie, Categorie, Hoofdcategorieën, Categorieën
+from .dag import Dag
 from .ingredient import Ingrediënt, Ingrediënten
 from .product import Product, Producten
 
