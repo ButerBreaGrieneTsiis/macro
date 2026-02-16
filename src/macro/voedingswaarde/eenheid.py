702425551e75200b4d6ea58155fb4a31c4e86eec
TomScheeper <tomscheeper@gmail.com>
2026-02-16 17:55:50 +0100

    Eenheid

---
+++ src/macro/voedingswaarde/eenheid.py
@@ -0,0 +1,45 @@
+"""macro.voedingswaarde.eenheid"""
+from __future__ import annotations
+from enum import Enum
+from typing import ClassVar, List
+
+
+class Eenheid(Enum):
+    
+    STUK = "stuk"
+    FLES = "fles", "flessen"
+    BLIK = "blik", "blikken"
+    POT = "pot", "potten"
+    PORTIE = "portie"
+    ZAK = "zak", "zakken"
+    THEELEPEL = "eetlepel"
+    EETLEPEL = "theelepel"
+    PLAK = "plak", "plakken"
+    VERPAKKING = "verpakking", "verpakkingen"
+    GRAM = "g", "gram"
+    MILLILITER = "ml", "milliliter"
+    KILOCALORIE = "kcal", "calorieën"
+    KILOJOULE = "kJ", "kilojoule"
+    
+    def __new__(
+        cls,
+        enkelvoud: str,
+        meervoud: str | None = None,
+        ) -> Eenheid:
+        veld = object.__new__(cls)
+        veld._value = enkelvoud
+        veld._meervoud = meervoud
+        return veld
+    
+    def __repr__(self) -> str:
+        return self.enkelvoud
+    
+    @property
+    def enkelvoud(self) -> str:
+        return self._value
+
+    @property
+    def meervoud(self) -> str:
+        if self._meervoud is None:
+            return self.enkelvoud + "s"
+        return self._meervoud
