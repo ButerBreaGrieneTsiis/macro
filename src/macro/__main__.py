82a7d6c7d596c77624f2533ef24e633bb0f5e400
TomScheeper <tomscheeper@gmail.com>
2026-02-19 23:36:28 +0100

    gestroomlijnde integratie van register openen/opslaan

--- src/macro/__main__.py
+++ src/macro/__main__.py
@@ -1,10 +1,5 @@
 """macro main entry point"""
-from grienetsiis.register import Register
-
 from macro.menu import hoofdmenu
-from macro.utils import welkomstscherm
 
-Register.openen()
-welkomstscherm()
+
 hoofdmenu()
-Register().opslaan()
--- src/macro/menu/hoofdmenu.py
+++ src/macro/menu/hoofdmenu.py
@@ -1,5 +1,12 @@
 """macro.menu.hoofdmenu"""
 from grienetsiis.opdrachtprompt import Menu
+from grienetsiis.register import Register
+
+from macro.utils import welkomstscherm
 
 
-hoofdmenu = Menu("HOOFDMENU")
+hoofdmenu = Menu(
+    naam = "HOOFDMENU",
+    functie_start = welkomstscherm,
+    functie_eind = Register.opslaan,
+    )
